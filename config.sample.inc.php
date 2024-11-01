b392ee5e86b4004bd28693eb60e102753d8a1e0a
bpx-chain <admin@bpxchain.cc>
2024-11-01 16:38:37 +0100

    Initial commit

---
+++ config.sample.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'bc_explorer_frontend');
+define('DB_PASS', 'password');
+define('DB_NAME', 'bc_explorer');
+
+define('EC_EXPLORER_URL', 'https://explorer.bpxchain.cc');
+
+?>
